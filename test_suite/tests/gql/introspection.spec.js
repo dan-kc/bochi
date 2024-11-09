7c0bf4e251266840c460b1e31e1fbfcad48bb2aa
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2024-11-09 20:07:46 +0000

    introspection test

---
+++ test_suite/tests/gql/introspection.spec.js
@@ -0,0 +1,24 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("Introspection", async ({ request }) => {
+  const query = {
+    query: `
+      {
+        __schema {
+          types {
+            name
+          }
+        }
+      }
+    `,
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+  expect(responseBody).toHaveProperty("data.__schema.types");
+});
