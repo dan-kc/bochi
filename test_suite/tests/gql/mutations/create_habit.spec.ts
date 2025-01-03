a313bda0361206ca236e7a11b2574db727f22fc4
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-01-03 18:49:20 +0100

    scaffold habit tests

---
+++ test_suite/tests/gql/mutations/create_habit.spec.ts
@@ -0,0 +1,390 @@
+import { expect } from "@playwright/test";
+import { createAccessToken, createStringOfLength, test } from "../../../helpers";
+
+test.beforeEach(async ({ db }) => {
+  await db.createUser();
+});
+
+test.afterEach(async ({ db }) => {
+  await db.deleteAllUsers();
+});
+
+function expectGoodHabit(responseBody: any) {
+  expect(responseBody.data.createHabit.id).toBeDefined();
+  expect(responseBody.data.createHabit.name).toBeDefined();
+  expect(responseBody.data.createHabit.difficulty).toBeDefined();
+  expect(responseBody.data.createHabit.created_at).toBeDefined();
+  expect(responseBody.data.createHabit.deleted_at).toBeDefined();
+  expect(responseBody.data.createHabit.hidden_until).toBeDefined();
+  expect(responseBody.data.createHabit.due_at).toBeDefined();
+  expect(responseBody.data.createHabit.importance).toBeDefined();
+  expect(responseBody.data.createHabit.duration).toBeDefined();
+}
+function expectError(responseBody: any, message: string) {
+  const errorMessages = responseBody.errors.map((error: any) => error.message);
+  expect(errorMessages).toContain(message);
+}
+
+const createHabitQuery = `
+  mutation CreateHabit($createHabitInput: CreateHabitInput) {
+    createHabit(input: $createHabitInput) {
+      id
+      name
+      difficulty
+      created_at
+      deleted_at
+      hidden_until
+      due_at
+      importance
+      duration
+    }
+  }
+`;
+
+test("Should create habit", async ({ request }) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 8,
+        importance: 5,
+        duration: 600,
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+  expectGoodHabit(responseBody);
+});
+
+test("Should create habit with description", async ({ request }) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 8,
+        importance: 5,
+        duration: 600,
+        description: "HI",
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+
+  expectGoodHabit(responseBody);
+  expect(responseBody.data.createHabit.description).toEqual("HI");
+});
+
+test("Should create hidden habit", async ({ request }) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 8,
+        importance: 5,
+        duration: 600,
+        hidden_until: "2024-12-16T00:33:08+08:00",
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+  expectGoodHabit(responseBody);
+});
+
+test("Should create due habit", async ({ request }) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 8,
+        importance: 5,
+        duration: 600,
+        due_date: "2024-12-16T00:33:08+08:00",
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+  expectGoodHabit(responseBody);
+});
+
+test("Should not create habit if name too long", async ({ request }) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: createStringOfLength(101),
+        difficulty: 8,
+        importance: 5,
+        duration: 600,
+        due_date: "2024-12-16T00:33:08+08:00",
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+
+  expectError(responseBody, "Name is too long. Must be fewer than 100 chars.");
+});
+
+test("Should not create habit if difficulty > 10", async ({ request }) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 11,
+        importance: 5,
+        duration: 600,
+        due_date: "2024-12-16T00:33:08+08:00",
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+
+  expectError(responseBody, "Difficulty can only be between 0 and 10");
+});
+
+test("Should not create habit if difficulty < 0", async ({ request }) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: -1,
+        importance: 5,
+        duration: 600,
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+
+  expectError(responseBody, "Difficulty can only be between 0 and 10");
+});
+
+test("Should not create habit if importance < 0", async ({ request }) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 8,
+        importance: -1,
+        duration: 600,
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+
+  expectError(responseBody, "Importance can only be between 0 and 10");
+});
+
+test("Should not create habit if importance > 10", async ({ request }) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 8,
+        importance: 11,
+        duration: 600,
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+
+  expectError(responseBody, "Importance can only be between 0 and 10");
+});
+
+test("Should not create habit if duration < 0", async ({ request }) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 8,
+        importance: 11,
+        duration: -5,
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+
+  expectError(responseBody, "Duration can't be negative");
+});
+
+test("Should not create habit with description if description too long.", async ({
+  request,
+}) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 8,
+        importance: 11,
+        duration: 5,
+        description: createStringOfLength(10001),
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+
+  expectError(
+    responseBody,
+    "Description too long. Must be fewer that 10000 chars.",
+  );
+});
+
+test("Should not create habit without valid access token", async ({
+  request,
+}) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 8,
+        importance: 11,
+        duration: 5,
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+
+  expectError(responseBody, "Invalid access token");
+});
+
+test("Should not create habit with expired access token", async ({
+  request,
+}) => {
+  const query = {
+    query: createHabitQuery,
+    variables: {
+      createHabitInput: {
+        name: "Touch grass",
+        difficulty: 8,
+        importance: 11,
+        duration: 5,
+      },
+    },
+  };
+
+  const response = await request.post("/graphql", {
+    data: query,
+    headers: {
+      Authorization: await createAccessToken(true),
+    },
+  });
+
+  expect(response.ok()).toBeTruthy();
+  const responseBody = await response.json();
+
+  expectError(responseBody, "Invalid access token");
+});
