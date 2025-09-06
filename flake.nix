34c5fd633b733795a31cfbb9538d41447d6320a6
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-09-06 12:23:13 +0100

    log either stdout or file

--- flake.nix
+++ flake.nix
@@ -37,7 +37,6 @@
             nixfmt-rfc-style
             taplo
             flyway
-            docker-compose
           ];
           shellHook = ''
             # If localstack and db is not running then start it
@@ -52,6 +51,7 @@
             export DATABASE_NAME="habit_market" # use "test_habit_market" in test runnners
             export RUST_BACKTRACE=1
             export RUST_LOG=trace
+            export LOG_DESTINATION=logs # If unset then it will stdout
           '';
         };
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use tracing_subscriber::EnvFilter;
 use tracing::info;
-
+use tracing_appender::rolling::hourly;
+use tracing_subscriber::EnvFilter;
 mod database;
 mod graphql;
 mod router;
@@ -10,12 +10,26 @@ mod security;
 
 #[tokio::main]
 async fn main() {
+    // Create a rolling file appender
+    // _guard has to alive for the duration of the application
+    let (writer, _guard) = match std::env::var("LOG_DESTINATION") {
+        Ok(dest) => {
+            let path = std::path::Path::new(&dest);
+            let file_prefix = "server.log";
+            let rolling_file_appender = hourly(path, file_prefix);
+            tracing_appender::non_blocking(rolling_file_appender)
+        }
+        Err(_) => tracing_appender::non_blocking(std::io::stdout()),
+    };
+
     tracing_subscriber::fmt()
+        .json()
         .with_env_filter(
             EnvFilter::try_from_default_env()
                 .or_else(|_| EnvFilter::try_new("habit_market_backend=error,tower_http=warn"))
                 .unwrap(),
         )
+        .with_writer(writer) // Use our conditional writer
         .init();
 
     let app = router::router().await;
