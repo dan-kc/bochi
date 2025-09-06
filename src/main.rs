f2fa19563b398106f45a53baa3aa48fade4cdb77
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-09-06 13:08:43 +0100

    log to both file and stout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use tracing::info;
 use tracing_appender::rolling::hourly;
-use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{EnvFilter, fmt, prelude::*};
 mod database;
 mod graphql;
 mod router;
@@ -10,27 +10,40 @@ mod security;
 
 #[tokio::main]
 async fn main() {
-    // Create a rolling file appender
-    // _guard has to alive for the duration of the application
-    let (writer, _guard) = match std::env::var("LOG_DESTINATION") {
+    // Always log to stdout
+    let stdout_layer = fmt::layer()
+        .json()
+        .with_writer(std::io::stdout);
+
+    // Conditionally add file logging
+    let registry = tracing_subscriber::registry()
+        .with(stdout_layer)
+        .with(
+            EnvFilter::try_from_default_env()
+                .or_else(|_| EnvFilter::try_new("habit_market_backend=error,tower_http=warn"))
+                .unwrap(),
+        );
+
+    // _guard has to be alive for the duration of the application
+    let _guard = match std::env::var("LOG_DESTINATION") {
         Ok(dest) => {
             let path = std::path::Path::new(&dest);
             let file_prefix = "server.log";
             let rolling_file_appender = hourly(path, file_prefix);
-            tracing_appender::non_blocking(rolling_file_appender)
+            let (non_blocking, guard) = tracing_appender::non_blocking(rolling_file_appender);
+            
+            let file_layer = fmt::layer()
+                .json()
+                .with_writer(non_blocking);
+            
+            registry.with(file_layer).init();
+            Some(guard)
         }
-        Err(_) => tracing_appender::non_blocking(std::io::stdout()),
+        Err(_) => {
+            registry.init();
+            None
+        }
     };
-
-    tracing_subscriber::fmt()
-        .json()
-        .with_env_filter(
-            EnvFilter::try_from_default_env()
-                .or_else(|_| EnvFilter::try_new("habit_market_backend=error,tower_http=warn"))
-                .unwrap(),
-        )
-        .with_writer(writer) // Use our conditional writer
-        .init();
 
     let app = router::router().await;
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
