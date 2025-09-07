eca521d8886a75672ce50ba8ce5fed80a30586e9
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-09-07 14:07:01 +0100

    rm playwright

--- test_suite/tests/healthcheck.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("healthcheck", async ({ request }) => {
-  const response = await request.get('/health');
-  const responseBody = await response.json();
-
-  expect(response.status()).toEqual(200);
-  expect(responseBody).toHaveProperty('healthy', true);
-});
