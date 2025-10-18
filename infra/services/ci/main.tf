6447dee9e0a2f40a6b94faa809a657a446952d9b
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2025-10-18 10:39:08 +0100

    oidc for circleci

--- infra/services/ci/main.tf
+++ infra/services/ci/main.tf
@@ -3,29 +3,41 @@ variable "container_registry_arn_list" {
   type        = list(string)
 }
 
-# Unique per AWS account. Already defined in keone.dev.
-data "aws_iam_openid_connect_provider" "github_actions" {
-  url = "https://token.actions.githubusercontent.com"
+locals {
+  circleci_org_id     = "2f633adc-140c-4ef2-818e-664307d1c0a9"
+  circleci_project_id = "80253561-a5bc-4229-8fa1-bcf2d834b9e2"
+}
+
+# Define the AWS IAM OIDC Provider for CircleCi
+# THIS IS UNIQUE PER ACCOUNT.
+resource "aws_iam_openid_connect_provider" "circleci" {
+  url = "https://oidc.circleci.com/org/${local.circleci_org_id}"
+  # List of audience claims, sts.amazonaws.com is required for AWS STS AssumeRole
+  client_id_list = [
+    "sts.amazonaws.com",
+    local.circleci_org_id,
+  ]
+
+  thumbprint_list = [
+    "20f540dd952c6054be3fc82f7a222a051df2b09b",
+  ]
 }
 
 # define an IAM Role in AWS that trusts this OIDC provider and grants the necessary permissions.
-resource "aws_iam_role" "github_actions_ecr_uploader" {
-  name = "github-actions-ecr-uploader"
+resource "aws_iam_role" "circleci_uploader_deployer" {
+  name = "circleci-uploader-deployer"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
         Principal = {
-          Federated = data.aws_iam_openid_connect_provider.github_actions.arn
+          Federated = aws_iam_openid_connect_provider.circleci.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
-          }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" : "repo:dan-kc/habit-market-backend:*"
+            "oidc.circleci.com/org/${local.circleci_org_id}:sub" : "org/${local.circleci_org_id}/project/${local.circleci_project_id}/user/*"
           }
         }
       }
@@ -34,9 +46,9 @@ resource "aws_iam_role" "github_actions_ecr_uploader" {
 }
 
 # Policy to interact with ECR
-resource "aws_iam_policy" "ecr_upload_policy" {
-  name        = "github-actions-ecr-upload-policy"
-  description = "Allows GitHub Actions to push images to ECR"
+resource "aws_iam_policy" "circleci_ecr_upload_policy" {
+  name        = "circleci-ecr-upload-policy"
+  description = "Allows CircleCi to push images to ECR"
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -67,6 +79,6 @@ resource "aws_iam_policy" "ecr_upload_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecr_upload_policy_attachment" {
-  role       = aws_iam_role.github_actions_ecr_uploader.name
-  policy_arn = aws_iam_policy.ecr_upload_policy.arn
+  role       = aws_iam_role.circleci_uploader_deployer.name
+  policy_arn = aws_iam_policy.circleci_ecr_upload_policy.arn
 }
