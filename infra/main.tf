1aeb9dba83668a889e42dfd347799199a26b303f
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2026-01-01 15:08:13 +0000

    rm infra

--- infra/main.tf
+++ infra/main.tf
@@ -57,50 +57,50 @@ resource "aws_ecr_repository" "tofustash_migration" {
 }
 
 # ECS Cluster
-resource "aws_ecs_cluster" "tofustash" {
-  name = "tofustash_cluster"
+# resource "aws_ecs_cluster" "tofustash" {
+#   name = "tofustash_cluster"
+#
+#   setting {
+#     name  = "containerInsights"
+#     value = "enabled"
+#   }
+# }
 
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-}
+# module "database" {
+#   source              = "./services/database"
+#   vpc_id              = aws_vpc.tofustash.id
+#   private_subnet_id   = aws_subnet.private.id
+#   private_subnet_b_id = aws_subnet.private_b.id
+#   private_subnet_cidr = aws_subnet.private.cidr_block
+# }
 
-module "database" {
-  source              = "./services/database"
-  vpc_id              = aws_vpc.tofustash.id
-  private_subnet_id   = aws_subnet.private.id
-  private_subnet_b_id = aws_subnet.private_b.id
-  private_subnet_cidr = aws_subnet.private.cidr_block
-}
+# module "ci" {
+#   source = "./services/ci"
+#   container_registry_arn_list = [
+#     aws_ecr_repository.tofustash_backend.arn,
+#     aws_ecr_repository.tofustash_migration.arn
+#   ]
+#   ecs_cluster_id             = aws_ecs_cluster.tofustash.id
+#   vpc_id                     = aws_vpc.tofustash.id
+#   private_subnet_ids         = [aws_subnet.private.id, aws_subnet.private_b.id]
+#   database_security_group_id = module.database.database_security_group_id
+#   migration_repository_url   = aws_ecr_repository.tofustash_migration.repository_url
+#   database_secret_arn        = module.database.database_secret_arn
+#   database_kms_key_arn       = module.database.database_kms_key_arn
+# }
 
-module "ci" {
-  source = "./services/ci"
-  container_registry_arn_list = [
-    aws_ecr_repository.tofustash_backend.arn,
-    aws_ecr_repository.tofustash_migration.arn
-  ]
-  ecs_cluster_id             = aws_ecs_cluster.tofustash.id
-  vpc_id                     = aws_vpc.tofustash.id
-  private_subnet_ids         = [aws_subnet.private.id, aws_subnet.private_b.id]
-  database_security_group_id = module.database.database_security_group_id
-  migration_repository_url   = aws_ecr_repository.tofustash_migration.repository_url
-  database_secret_arn        = module.database.database_secret_arn
-  database_kms_key_arn       = module.database.database_kms_key_arn
-}
-
-module "server" {
-  source = "./services/server"
-
-  vpc_id                     = aws_vpc.tofustash.id
-  private_subnet_ids         = [aws_subnet.private.id, aws_subnet.private_b.id]
-  ecs_cluster_id             = aws_ecs_cluster.tofustash.id
-  ecs_cluster_name           = aws_ecs_cluster.tofustash.name
-  database_security_group_id = module.database.database_security_group_id
-  database_secret_arn        = module.database.database_secret_arn
-  database_kms_key_arn       = module.database.database_kms_key_arn
-  database_endpoint          = module.database.database_endpoint
-  ecr_repository_url         = aws_ecr_repository.tofustash_backend.repository_url
-  alb_security_group_id      = aws_security_group.alb.id
-  https_listener_arn         = aws_lb_listener.https.arn
-}
+# module "server" {
+#   source = "./services/server"
+#
+#   vpc_id                     = aws_vpc.tofustash.id
+#   private_subnet_ids         = [aws_subnet.private.id, aws_subnet.private_b.id]
+#   ecs_cluster_id             = aws_ecs_cluster.tofustash.id
+#   ecs_cluster_name           = aws_ecs_cluster.tofustash.name
+#   database_security_group_id = module.database.database_security_group_id
+#   database_secret_arn        = module.database.database_secret_arn
+#   database_kms_key_arn       = module.database.database_kms_key_arn
+#   database_endpoint          = module.database.database_endpoint
+#   ecr_repository_url         = aws_ecr_repository.tofustash_backend.repository_url
+#   alb_security_group_id      = aws_security_group.alb.id
+#   https_listener_arn         = aws_lb_listener.https.arn
+# }
